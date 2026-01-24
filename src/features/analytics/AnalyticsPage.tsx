ff3cb5a023005654a640ef45063efab7dae3aec5
Hemrajjj13 <iamhemraj13@gmail.com>
2026-01-24 23:26:55 +0530

    created dummy anylytics page

---
+++ src/features/analytics/AnalyticsPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { MainLayout } from "../../components/layout/MainLayout";
+
+export const AnalyticsPage = () => {
+  return (
+    <MainLayout>
+      <h1>Analytics</h1>
+      <p>Analytics data will appear here.</p>
+    </MainLayout>
+  );
+};
